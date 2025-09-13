b88ed9ab928a4446eb33d7cfd688be11106c4af6
burkulsachin98 <sachinburkul98@gmail.com>
2025-09-13 22:49:37 +0530

    13 sept Class Commit

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
+    }
+
+
+}
---
+++ src/ex_01_Java_Basics/Lab0021_Boolean.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab0021_Boolean
+{
+    public static void main(String[] args) {
+        boolean is_sachin_pass = true;
+        boolean sachin_has_audi = false;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+
+
+    }
+}
