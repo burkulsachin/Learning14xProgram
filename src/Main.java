5a319f72e06bc004845bba0d2c77ac271fc50451
burkulsachin98 <sachinburkul98@gmail.com>
2025-09-13 22:53:27 +0530

    13 sept Class Commit 2

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,16 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
 
+            System.out.println(10/3);
 
+
+
+
+
+            /*  ctrl + shift+/
+*
+*
+*  */
 
 
 
