7903f1413f9db2ce4ba57c37098dff8329e949f3
sachin.burkul <sachinburkul2006@gmaiol.com>
2025-12-05 10:11:14 +0530

    5 dec 1

--- src/ex_29_collection_framework/CF_01_List/CF_04_MAP_Copy/Lab0190_Map_iterate.java
+++ src/ex_29_collection_framework/CF_01_List/CF_04_MAP_Copy/Lab0190_Map_iterate.java
@@ -1,21 +1,21 @@
-package ex_29_collection_framework.CF_01_List.CF_04_MAP_Copy;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Lab0190_Map_iterate {
-    Map<String, Integer> map = new HashMap();
-        map.put("id", 1);
-        map.put("id2", 2);
-        map.put("id3", 34);
-        map.put("id4", null);
-        map.put("id5", null);
-        map.put(null, 100);
-
-     //   System.out.println(map);
-        System.out.println(map.size());
-
-        //for (Map.Entry<String, Integer> item : map.entrySet()) {
-        //System.out.println(item.getKey() + " -> " + item.getValue());
-    }
-}
+//package ex_29_collection_framework.CF_01_List.CF_04_MAP_Copy;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Lab0190_Map_iterate {
+//    Map<String, Integer> map = new HashMap();
+//        map.put("id", 1);
+//        map.put("id2", 2);
+//        map.put("id3", 34);
+//        map.put("id4", null);
+//        map.put("id5", null);
+//        map.put(null, 100);
+//
+//     //   System.out.println(map);
+//        System.out.println(map.size());
+//
+//        //for (Map.Entry<String, Integer> item : map.entrySet()) {
+//        //System.out.println(item.getKey() + " -> " + item.getValue());
+//    }
+//}
